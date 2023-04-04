650178794dc998c318a37c1a783beffc821ec2ad
HypnoticSiege <luis@hypnoticsiege.net>
2023-04-04 10:10:24 -0400

    connect to socket on client

--- public/js/solver.js
+++ public/js/solver.js
@@ -1,1 +1,11 @@
-console.log("Solver.js loaded")
+const socket = new WebSocket('ws://localhost:8080');
+
+socket.onopen = () => { console.log('Connected to WebSocket Server') };
+socket.onclose = () => { console.log('Disconnected from WebSocket Server') };
+socket.onerror = (error) => { console.log('Error connecting to WebSocket Server', error) };
+
+socket.onmessage = (message) => {
+    const data = JSON.parse(message.data);
+    console.log(data);
+
+};
