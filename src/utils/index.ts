fb583c56253ed6954af02361f81f201252a6dc7b
HypnoticSiege <luis@hypnoticsiege.net>
2023-04-04 10:16:26 -0400

    replace weird chars

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,5 @@
 function stripHTML(html: string) {
-    return html.replace(/<[^>]*>/g, "").replace(/\s\s+/g, ' ');
+    return html.replace(/<[^>]*>/g, "").replace(/&nbsp;/g, ' ').trim().replace(/\s\s+/g, ' ');
 }
 
 export default {
