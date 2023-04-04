cfbcfa61c5687ed9c98686d0165fe61d02a51e4f
HypnoticSiege <luis@hypnoticsiege.net>
2023-04-04 11:38:53 -0400

    create completion on the ws server res

--- src/sockets/helpers/solve.ts
+++ src/sockets/helpers/solve.ts
@@ -1,9 +1,25 @@
 import utils from "../../utils";
+import openai from "../../openai";
 
 async function solve(data, socket) {
+    const completion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+            {
+                role: 'user',
+                content: 'You are MapGPT, a model that will answer MAP Testing questions. Please respond to the following question with nothing but the answer.'
+            },
+            {
+                role: 'user',
+                content: utils.stripHTML(data.question)
+            }
+        ]
+    })
+
+
     await socket.send(JSON.stringify({
         type: "solve",
-        message: utils.stripHTML(data.question)
+        message: completion.data.choices[0].message.content
     }))
 };
 
